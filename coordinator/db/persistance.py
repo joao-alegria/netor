cc867286040be638375abf8072374c257e23656c
joao-alegria <joaops.alegria@gmail.com>
2021-04-08 17:28:02 +0100

    added databases autocreation

--- coordinator/db/persistance.py
+++ coordinator/db/persistance.py
@@ -209,10 +209,19 @@ class NetworkSliceInstanceVnfPlacement(Base):
 
 
 engine = create_engine('postgresql://'+str(config.POSTGRES_USER)+':'+str(config.POSTGRES_PASS)+'@'+str(config.POSTGRES_IP)+':'+str(config.POSTGRES_PORT)+'/'+str(config.POSTGRES_DB))
-Base.metadata.create_all(engine)
+try:
+  Base.metadata.create_all(engine)
+except Exception as e:
+  if "does not exist" in str(e):
+    tmpengine=create_engine('postgresql://'+str(config.POSTGRES_USER)+':'+str(config.POSTGRES_PASS)+'@'+str(config.POSTGRES_IP)+':'+str(config.POSTGRES_PORT)+'/postgres')
+    conn = tmpengine.connect()
+    conn.execute("commit")
+    conn.execute("create database \""+str(config.POSTGRES_DB)+"\"")
+    conn.close()
+    Base.metadata.create_all(engine)
+
 Session = sessionmaker(bind=engine)
 session=Session()
-
 def persist(entity):
   session.add(entity)
   session.commit()
